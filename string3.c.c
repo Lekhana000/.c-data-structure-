e1ec80d0020be84bf225c73408700707e72d2382
Lekhana000 <lekhanabasavaraj18@gmail.com>
2024-09-12 09:14:20 +0530

    Add files via upload

---
+++ string3.c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+  int n;
+  char str[10];
+  printf("enter your name:");
+  scanf("%s",str);
+  printf("enter your age:");
+  
+  
+  
+  }
---
+++ string4.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+  int n;
+  char str[10];
+  printf("enter your name:");
+  scanf("%s",str);
+ n=strlen(str);
+  printf("Hello %s, Welcome to c programming",str);
+  
+  
+  
+  
+  }
